c1ac43d5cf1d0f747b3e880d14ff4a86ffb89cf6
mpuigomatech <mpuig@omatech.com>
2023-10-08 22:13:40 +0200

    Added missed files

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var');
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+    ])
+    ->setFinder($finder);
